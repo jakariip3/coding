b424265b9c5e957de9d3b14711425014f1ac292a
jakariip3 <132089309+jakariip3@users.noreply.github.com>
2026-01-08 14:48:58 -0800

    Update with.py

--- with.py
+++ with.py
@@ -4,3 +4,16 @@ with open("example.txt", "r") as file:
     contents = file.read()
 
 print(contents)
+
+
+with open("example.txt", "r") as file:
+    for line in file:
+        print(line.strip())
+
+
+with open("example.txt", "r") as file:
+    lines = file.readlines()
+
+with open("output.txt", "w") as output:
+    for line in lines:
+        output.write(line.upper())
